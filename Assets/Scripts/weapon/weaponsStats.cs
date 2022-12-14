425f88201bb41efd173d8011fe2cb5d6c81eb316
WIN-1OKU51VBEEP\Admin <yashagostov626@gmail.com>
2022-12-14 19:13:51 +0500

    Hren kakayato sdelana

---
+++ Assets/Scripts/weapon/weaponsStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponsStats : MonoBehaviour
+{
+    public int _damage;
+
+    public int _range;
+
+    public int _attackTimeInterval;
+
+}
