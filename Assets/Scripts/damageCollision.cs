dc98a27ae725b3c476ee62b4d0fb45f3177effe8
Yashagostov <yashagostov626@gmail.com>
2022-12-15 22:32:40 +0500

    Enemy take damage, destroy enemy, (whith ettentions)

--- Assets/Scripts/damageCollision.cs
+++ Assets/Scripts/damageCollision.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class damageCollision : MonoBehaviour
 {
-    public int _collisionDamage;
+    public int _collisionDamage = 10;
 
     public string _collisionTag;
 
+    private CarCondition carCondition;
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        carCondition = GetComponent<CarCondition>();
+        
+        
+    
         if (collision.gameObject.tag == _collisionTag)
         {
-            _collisionDamage = collision.gameObject.GetComponent<WeaponsStats>()._damage;
-            CarCondition _currentHealth = collision.gameObject.GetComponent<CarCondition>();
+            GetComponent<CarCondition>().TakeDamage(_collisionDamage);
+         //   carCondition
+           // _collisionDamage = collision.gameObject.GetComponent<WeaponsStats>()._damage;
+           // CarCondition _currentHealth = collision.gameObject.GetComponent<CarCondition>();
             //_currentHealth.TakeDamage();
-            _currentHealth.TakeDamage(_collisionDamage);
+          //  _currentHealth.TakeDamage(_collisionDamage);
         }
     }
 }
