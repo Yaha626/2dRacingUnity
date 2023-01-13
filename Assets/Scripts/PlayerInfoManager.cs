9cf67a999e7b90bd1403321ae70699ab5c9ee5d8
WIN-1OKU51VBEEP\Admin <yashagostov626@gmail.com>
2023-01-13 19:20:50 +0500

    add text-device type. delite

--- Assets/Scripts/PlayerInfoManager.cs
+++ Assets/Scripts/PlayerInfoManager.cs
@@ -9,53 +9,16 @@ public class PlayerInfoManager : MonoBehaviour
 
     public static int _SelectedCar;
 
-    [SerializeField] Text _PlayerNameFromYandex;
-    
-    [SerializeField] ImageLoadYG _PlayerIconFromYandex;
 
     [SerializeField] Text _PlayerDeviceFromYandex;
 
-    private void OnEnable() => YandexGame.GetDataEvent += DebugData;
-    private void OnDisable() => YandexGame.GetDataEvent -= DebugData;
 
 
 
     private void Start()
     {
-        if (YandexGame.SDKEnabled)
-        {
-            DebugData();
-        }
+        _PlayerDeviceFromYandex.text = YandexGame.EnvironmentData.deviceType;
     }
 
-    public void  DebugData()
-    {
-        if (_PlayerIconFromYandex != null && YandexGame.auth)
-            _PlayerIconFromYandex.Load(YandexGame.playerPhoto);
-
-        _PlayerDeviceFromYandex.text = "domain - " + YandexGame.EnvironmentData.domain +
-              "\ndeviceType - " + YandexGame.EnvironmentData.deviceType +
-              "\nisMobile - " + YandexGame.EnvironmentData.isMobile +
-              "\nisDesktop - " + YandexGame.EnvironmentData.isDesktop +
-              "\nisTablet - " + YandexGame.EnvironmentData.isTablet +
-              "\nisTV - " + YandexGame.EnvironmentData.isTV +
-              "\nisTablet - " + YandexGame.EnvironmentData.isTablet +
-              "\nappID - " + YandexGame.EnvironmentData.appID +
-              "\nbrowserLang - " + YandexGame.EnvironmentData.browserLang +
-              "\npayload - " + YandexGame.EnvironmentData.payload +
-              "\npromptCanShow - " + YandexGame.EnvironmentData.promptCanShow +
-              "\nreviewCanShow - " + YandexGame.EnvironmentData.reviewCanShow;
-
-        string playerId = YandexGame.playerId;
-        if (playerId.Length > 7)
-            playerId = playerId.Remove(7) + "...";
-
-        _PlayerNameFromYandex.text = "playerName - " + YandexGame.playerName +
-               "\nplayerId - " + playerId +
-               "\nauth - " + YandexGame.auth +
-               "\nSDKEnabled - " + YandexGame.SDKEnabled +
-               "\ninitializedLB - " + YandexGame.initializedLB +
-               "\nphotoSize - " + YandexGame.photoSize;
-    }
 
 }
