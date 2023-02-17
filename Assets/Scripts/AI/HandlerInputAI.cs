47f4282822139ee89f5909ff4b54c4fab82908e2
WIN-1OKU51VBEEP\Admin <yashagostov626@gmail.com>
2023-02-17 19:05:46 +0500

    ---

--- Assets/Scripts/AI/HandlerInputAI.cs
+++ Assets/Scripts/AI/HandlerInputAI.cs
@@ -12,17 +12,29 @@ public class HandlerInputAI : MonoBehaviour
 
     Vector3 _targetPosition = Vector3.zero;
 
+  //  Vector2 _targetWayRoad = Vector2.zero;
+
     Transform _targetTransform = null;
 
     CarController1Player _CarController;
 
-    public GameObject _rayMainFront;
+    public GameObject _LeftTurnPoint;
 
-    public GameObject _rayMainFrontPointDirection;
+    public GameObject _RightTurnPoint;
+
+    public GameObject _StraightWay;
+
+    public GameObject _StraightWayHome;
+
+    // private GameObject _tmpTurnSelector;
+
+    public LayerMask _layerMaskBorderOfRoadMainRay;
 
     [SerializeField] float rayMainFrontDistance;
  
-    float _actionOnDistance;
+   // float _actionOnDistance;
+
+    
 
 
 
@@ -38,27 +50,57 @@ public class HandlerInputAI : MonoBehaviour
 
     private void Start()
     {
-        _actionOnDistance = 0f;
+       // _actionOnDistance = 0f;
     }
 
 
     private void Update()
     {
 
-      RaycastHit2D _hitObstance =   Physics2D.Raycast(_rayMainFront.transform.position, Vector2.up , rayMainFrontDistance);
+        RaycastHit2D _hitLeftSide = Physics2D.Raycast(_LeftTurnPoint.transform.position, transform.up, 10, _layerMaskBorderOfRoadMainRay);
 
-        if(_hitObstance.collider != null)
+
+
+        RaycastHit2D _hitRigytSide = Physics2D.Raycast(_RightTurnPoint.transform.position, transform.up, 10, _layerMaskBorderOfRoadMainRay);
+
+        if(!_hitLeftSide & !_hitRigytSide)
+        {
+            _StraightWay.transform.position = _StraightWayHome.transform.position;
+        }
+
+        if (_hitLeftSide)
+        {
+            _StraightWay.transform.position = _RightTurnPoint.transform.position;
+        }
+
+        if (_hitRigytSide)
+        {
+            _StraightWay.transform.position = _LeftTurnPoint.transform.position;
+        }
+        
+
+
+
+        Debug.DrawRay(_LeftTurnPoint.transform.position, transform.up * 10, Color.green);
+        Debug.DrawRay(_RightTurnPoint.transform.position, transform.up * 10, Color.green);
+
+        RaycastHit2D _hitObstance =   Physics2D.Raycast(transform.position, transform.up, rayMainFrontDistance, _layerMaskBorderOfRoadMainRay);
+
+
+        if(_hitObstance)
         {
             Debug.Log("Ray is Detect Object");
+
             Debug.DrawRay(transform.position, transform.up * rayMainFrontDistance, Color.green); // this is good!
 
-           // Debug.DrawRay(_rayMainFront.transform.position, Vector2.up, rayMainFrontDistance, Color.yellow);
+           // Destroy(_hitObstance.collider.gameObject);
 
         }
         else
         {
             Debug.Log("Ray is not Detect Object");
-            Debug.DrawRay(_rayMainFront.transform.position, _rayMainFrontPointDirection.transform.position, Color.red);
+
+           // Debug.DrawRay(); // this is good!
         }
 
     }
@@ -83,7 +125,7 @@ public class HandlerInputAI : MonoBehaviour
 
         inputVector.x = TurnTowardTarget();
 
-        inputVector.y = 0.05f;
+        inputVector.y = 0.2f;
 
         _CarController.SetInputVector(inputVector);
 
@@ -109,10 +151,11 @@ public class HandlerInputAI : MonoBehaviour
    private void FollowWayPoints()
     {
 
+        
         if (_targetTransform == null)
         {
 
-         //   _targetTransform = _wayCorrector.transform;
+             _targetTransform = _StraightWay.transform;
 
         }
 
@@ -122,7 +165,7 @@ public class HandlerInputAI : MonoBehaviour
             _targetPosition = _targetTransform.position;
 
         }
-
+      
     }
 
 
